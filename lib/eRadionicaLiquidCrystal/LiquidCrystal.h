ccb757839d771f834abe6ce3a9bef297463776c8
mjosip72 <josip.mojzes12345@gmail.com>
2021-12-21 19:57:56 +0100

    undo_this

--- lib/eRadionicaLiquidCrystal/LiquidCrystal.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include "LiquidCrystal_I2C.h"
-
-class LiquidCrystal : public LiquidCrystal_I2C {
-   public:
-   LiquidCrystal() : LiquidCrystal_I2C(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE) {}
-};
